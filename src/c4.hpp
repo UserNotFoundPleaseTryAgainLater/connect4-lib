0008960182b6b0458ce5806ac456d7935ab90eaa
UserNotFoundPleaseTryAgainLater <tpark@nastudents.org>
2025-09-09 11:25:52 +0000

    Made isValidPosition method private

--- src/c4.hpp
+++ src/c4.hpp
@@ -1,6 +1,7 @@
 #pragma once
-#include <array>
 #include <iostream>
+#include <vector>
+#include <array>
 
 namespace c4
 {
@@ -8,8 +9,6 @@ namespace c4
     {
     private:
         std::array<std::array<short, 7>, 6> board{};
-    public:
-        Board() = default;
 
         bool isValidPosition(const std::array<std::array<short, 7>, 6>& pos)
         {
@@ -32,17 +31,20 @@ namespace c4
             return true;
         }
 
+    public:
+        Board() = default;
+        
         void clear()
         {
             board = {}; // resets everything to 0
         }
-
+        
         void set(const std::array<std::array<short, 7>, 6>& newPos)
         {
             if (isValidPosition(newPos))
                 board = newPos;
         }
-
+        
         void show()
         {
             std::cout << "\n";
@@ -55,23 +57,9 @@ namespace c4
                     else if (board[row][column] == 1)
                         std::cout << "\033[1;93m\u25C9\033[0m ";
                     else if (board[row][column] == 0)
-                        std::cout << "  ";
+                        std::cout << " ";
                 }
                 std::cout << "\n";
-            }
-        }
-    };
-
-    class Move
-    {
-    private:
-        int column;
-    public:
-        Move(int move)
-        {
-            if (move >= 1 && move <= 7)
-            {
-                column = move - 1;
             }
         }
     };
