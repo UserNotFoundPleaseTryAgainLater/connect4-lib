fca583b7b2dae0843e8cf3d28d0df55a057eaa75
UserNotFoundPleaseTryAgainLater <tpark@nastudents.org>
2025-09-14 13:33:43 +0000

    Fixed minor bugs

--- src/c4.hpp
+++ src/c4.hpp
@@ -20,20 +20,20 @@ namespace c4
         std::bitset<42> bitboardYellow;
         std::bitset<42> bitboardRed;
     public:
-        toBits(Board board)
+        void toBits(Board board, std::bitset<42>& bitsYellow, std::bitset<42>& bitsRed)
         {
-            for (int column = 0; column < 7; column++;)
+            for (int column = 0; column < 7; column++)
             {
                 for (int row = 0; row < 6; row++)
                 {
                     if ((board.getElement[row][column]) == 1)
                     {
-                        int bitIndex = 7 * row + column;
+                        int bitIndex = 6 * column + row;
                         bitboardYellow.set(bitIndex);
                     }
                     else if ((board.getElement[row][column]) == -1)
                     {
-                        int bitIndex = 7 * row + column;
+                        int bitIndex = 6 * column + row;
                         bitboardRed.set(bitIndex);
                     }
                 }
